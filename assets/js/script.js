55d595ecdca5247a821dd9e47cfdd43488e6a359
Fidel Maulto <fidel.maluto77@gmail.com>
2026-05-16 22:22:50 +0100

    refactor: Mudando os IDs

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,15 +7,15 @@ menuToggle.addEventListener('click', () => {
 
 // Filter de históricos
 const search = document.getElementById('searchInput');
-const tabela = document.getElementById('historicoTable');
+const tabela = document.getElementById('filtrando');
 const linhas = tabela.getElementsByTagName('tr');
 
-search.addEventListener('keyup' , () => {
+search.addEventListener('keyup', () => {
   const filtro = search.value.toLowerCase();
 
   for (let i = 0; i < linhas.length; i++) {
     const coluna = linhas[i].getElementsByTagName('td')[1];
-    if(coluna) {
+    if (coluna) {
       const texto = coluna.textContent.toLowerCase();
 
       linhas[i].style.display = texto.includes(filtro) ? "" : "none";
