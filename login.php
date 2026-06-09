d34b84c773408b42f091eb8ed61320bfc2c942a5
Fidel Maulto <fidel.maluto77@gmail.com>
2026-06-09 06:12:25 +0100

    refactor: Eliminar o botão a mais da tela de login

--- login.php
+++ login.php
@@ -66,8 +66,6 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
       </div>
 
       <button type="submit" class="btn-login">Entrar</button>
-          
-      <button type="submit" class="btn-login">Entrar</button>
 
     </form>
 
